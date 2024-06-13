a1e27975150db904b8e0bd2c50277f77075cf322
Confidence Emonena <confidinho@yahoo.com>
2024-06-13 16:30:36 +0100

    auth form

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 function MobileNav({ user }: MobileNavProps) {
   const pathName = usePathname();
@@ -84,7 +85,7 @@ function MobileNav({ user }: MobileNavProps) {
                 USER
               </nav>
             </SheetClose>
-            FOOTER
+            <Footer user={user} />
           </div>
         </SheetContent>
       </Sheet>
