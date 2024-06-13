59d5e7a7f7c55a5f713529da5611348480e42632
Confidence Emonena <confidinho@yahoo.com>
2024-06-13 16:23:13 +0100

    auth form

--- components/RIghtSidebar.tsx
+++ components/RIghtSidebar.tsx
@@ -47,7 +47,7 @@ const RIghtSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               />
             </div>
             {banks[1] && (
-              <div className="absolute right-0 top-8 z-0 w-[90%]">
+              <div className="absolute right-[-5%] top-8 z-0 w-[95%]">
                 <BankCard
                   key={banks[1].$id}
                   account={banks[1]}
